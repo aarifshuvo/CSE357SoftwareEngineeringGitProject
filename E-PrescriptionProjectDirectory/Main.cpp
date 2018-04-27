8477f0070d27add04d47ef657ab34dc5167699af
GM Ariful Islam <aarifshuvo@gmail.com>
2018-04-27 20:54:32 +0600

    Project Skull Methods Created in Cpp.

---
+++ E-PrescriptionProjectDirectory/Main.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct DT
+{
+    string medicine_name;
+    int morning_dose, noon_dose, night_dose;
+};
+
+void AddMedication()
+{
+
+}
+
+
+void editDose()
+{
+
+}
+
+void checkFormulary()
+{
+
+}
+
+void AuditPrescriptionToDatabase()
+{
+
+}
+
+int main()
+{
+
+
+    return 0;
+}
